331f768974af601ac81cae457aa31794ae0d559e
Roman Pronskiy <roman@pronskiy.com>
2020-02-20 16:34:53 +0200

    EAP 4 meta arrayaccess

---
+++ src/metadata/ArrayAccess/arrayaccess_meta.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace metadata\ArrayAccess;
+
+class ServiceManager implements ArrayAccess
+{
+    public function offsetExists($offset)
+    {
+        // TODO: Implement offsetExists() method.
+    }
+    public function offsetGet($offset)
+    {
+        // TODO: Implement offsetGet() method.
+    }
+    public function offsetSet($offset, $value)
+    {
+        // TODO: Implement offsetSet() method.
+    }
+    public function offsetUnset($offset)
+    {
+        // TODO: Implement offsetUnset() method.
+
+
+    }
+}
+
+$serviceManager = (new ServiceManager());
+
+$serviceManager[''];
