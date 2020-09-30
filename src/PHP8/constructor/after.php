828e19199d71b56d7a2bdfe0af188af4870ce0ca
Roman Pronskiy <roman@pronskiy.com>
2020-09-30 12:02:56 +0300

    Constructor

---
+++ src/PHP8/constructor/after.php
@@ -0,0 +1,12 @@
+<?php
+// PHP 8
+
+class Point
+{
+    public function __construct(
+        public float $x = 0.0,
+        public float $y = 0.0,
+        public float $z = 0.0,
+    ) {
+    }
+}
---
+++ src/PHP8/constructor/before.php
@@ -0,0 +1,19 @@
+<?php
+// PHP ≤ 7.4
+
+class Point
+{
+    public float $x;
+    public float $y;
+    public float $z;
+
+    public function __construct(
+        float $x = 0.0,
+        float $y = 0.0,
+        float $z = 0.0,
+    ) {
+        $this->x = $x;
+        $this->y = $y;
+        $this->z = $z;
+    }
+}
