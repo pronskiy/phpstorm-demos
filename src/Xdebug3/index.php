b76d2bd5f87c2cc045d4950b0756c8e481a4ed8d
Roman Pronskiy <roman@pronskiy.com>
2020-12-01 20:45:38 +0200

    2020.3 Xdebug

--- src/Xdebug3/index.php
+++ src/Xdebug3/index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo 'Hello world!';
+class DTO {
+    public function __construct(
+        public string $prop
+    ) { }
+}
+
+$dtos = [
+    new DTO('Xdebug 3'),
+    new DTO('PhpStorm'),
+    new DTO('PHP 8'),
+];
+
+foreach ($dtos as $dto) {
+    echo $dto->prop;
+}
