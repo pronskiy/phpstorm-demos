0622cbe06008ed12f7ee5b5575a63d85dac927ea
Roman Pronskiy <roman@pronskiy.com>
2021-03-04 15:43:37 +0200

    Add if_extensions is loaded

--- src/Inspections/imagick.php
+++ src/Inspections/imagick.php
@@ -1,8 +1,10 @@
 <?php
 
+if (extension_loaded('imagick')) {
     $im = new Imagick();
     try {
         $im->setResolution(300, 300);
-        $im->readImage( "test.pdf" );
+        $im->readImage("test.pdf");
     } catch (ImagickException $e) {
     }
+}
