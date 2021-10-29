c0e2ace75fb7beb227655b4b7618f13853d59379
Roman Pronskiy <roman@pronskiy.com>
2021-10-29 14:51:49 +0300

    2021.3 EAP#6

--- src/PHP81/first-class_callables.php
+++ src/PHP81/first-class_callables.php
@@ -1,18 +1,30 @@
 <?php
+class Foo {
+    public string $property = '';
+}
+$obj = new Foo();
+$methodStr = '';
+$classStr = 'Foo';
 
-// Можно сделать closure из любого callable
-function foo($a, $b) { }
-$closure = foo(...);
-$closure('a', 'b');
+    // Create closure out of any callable
+    function foo($a, $b) { }
+    $closure = foo(...);
+    $closure('a', 'b');
 
-// Работает со всеми вариантами синтаксиса
-$fn = strlen(...);
-$fn = $obj->method(...);
-$fn = Foo::method(...);
-$fn = [$obj, 'method'](...);
-$fn = ($obj->property)(...);
-$fn = $obj->$methodStr(...);
-$fn = $classStr::$methodStr(...);
+    // Works with all syntax variants
+    $fn = strlen(...);
+    $fn = $obj->method(...);
+    $fn = Foo::method(...);
+    $fn = [$obj, 'method'](...);
+    $fn = ($obj->property)(...);
+    $fn = $obj->$methodStr(...);
+    $fn = $classStr::$methodStr(...);
 
-// Пример использования
-array_map(strval(...), [1, 2, 3]);
+    // Example of usage
+    array_map(strval(...), [1, 2, 3]);
+
+
+
+
+    $fn = strlen(...);
+
