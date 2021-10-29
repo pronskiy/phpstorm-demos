b437ff13e8a81a4b6c076eee5d6e1893f3055994
Roman Pronskiy <roman@pronskiy.com>
2021-10-29 14:51:34 +0300

    2021.3 EAP#5

--- src/PHP81/deprecations.php
+++ src/PHP81/deprecations.php
@@ -1,24 +1,37 @@
 <?php
 
-$GLOBALS = [];
-$GLOBALS += [];
-$GLOBALS =& $x;
-$x =& $GLOBALS;
-unset($GLOBALS);
+    $GLOBALS = [];
+    $GLOBALS += [];
+    $GLOBALS =& $x;
+    $x =& $GLOBALS;
+    unset($GLOBALS);
 
-class Foo implements Serializable
-{
-    public function serialize()
+
+
+
+
+    class Foo implements Serializable
     {
-        // TODO: Implement serialize() method.
+        public function __serialize(): array
+        {
+            // TODO: Implement __serialize() method.
+        }
+
+        public function __unserialize(array $data): void
+        {
+            // TODO: Implement __unserialize() method.
+        }
     }
 
-    public function unserialize(string $data)
-    {
-        // TODO: Implement unserialize() method.
-    }
 
-}
+
+
+
+
+
+
+
+
 
 
 class MyDateTime extends DateTime
@@ -40,3 +53,71 @@ var_dump($array);
 
 
 $t = strlen(null);
+
+var_dump(str_contains("foobar", null));
+
+$a = [];
+$a[15.5]; // deprecated, as key value loses the 0.5 component
+$a[15.0]; // ok, as 15.0 == 15
+
+function &test(): void {}
+
+$arr = false;
+$arr[] = 2;// deprecated
+
+mb_check_encoding();
+odbc_result_all();
+
+
+$array1 = ["a" => 1];
+
+$array2 = ["b" => 2];
+
+$array = ["a" => 0, ...$array1, ...$array2];
+
+$s = strstr(null, "\n");
+
+
+class Foo34
+{
+    public function bar(): array
+    {
+    }
+}
+
+class Suppress extends Foo34
+{
+
+    public function bar(): int
+    {
+        return 25;
+    }
+}
+
+class Foo implements ArrayAccess {
+    #[\ReturnTypeWillChange]
+    public function offsetGet(mixed $offset) {}
+    // ...
+}
+
+function canReturnFalse() {
+    return false;
+}
+
+
+    $arr = canReturnFalse();
+    // naively try to append
+    $arr[] = ['new row values'];
+
+
+
+
+
+
+
+
+
+
+
+
+
